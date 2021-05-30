9b6676dcd4bf48b7a8b438636230daaad52dcf36
yasfu <fumiaki2381@gmail.com>
2021-05-31 00:28:12 +0900

    バリデーションとアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
+  has_many :likes
+  has_many :reviews
+  has_many :hotels
 end
