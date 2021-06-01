2da224157ea0d2899da8f0c7c955bd41e6484423
yasfu <fumiaki2381@gmail.com>
2021-06-01 14:39:48 +0900

    アクションの設定

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,4 +1,24 @@
 class HotelsController < ApplicationController
   def index
+    @hotels = Hotel.order("created_at DESC")
+  end
+
+  def new
+    @hotel = Hotel.new
+  end
+
+  def create
+    @hotel = Hotel.new(hotel_params)
+    if @hotel.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def hotel_params
+    params.require(:hotel).permit(:name, :category_id)
   end
 end
