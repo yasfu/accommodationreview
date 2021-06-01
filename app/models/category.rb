1a87402245b737fe3aa8a2487f999bcf1f26b9a3
yasfu <fumiaki2381@gmail.com>
2021-06-01 14:39:18 +0900

    モデルの作成

---
+++ app/models/category.rb
@@ -0,0 +1,17 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'レジャー' },
+    { id: 3, name: 'ブライダル' },
+    { id: 4, name: '近隣施設' },
+    { id: 5, name: '海' },
+    { id: 6, name: '和風' },
+    { id: 7, name: 'グルメ' },
+    { id: 8, name: 'イベント' },
+    { id: 9, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :hotels
+
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_130339) do
+ActiveRecord::Schema.define(version: 2021_05_31_123624) do
+
+  create_table "hotels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
