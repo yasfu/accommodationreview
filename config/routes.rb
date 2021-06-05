c1a34d7cc43132ba21f1851dbb2f94aea3f2d106
yasfu <fumiaki2381@gmail.com>
2021-06-06 02:02:57 +0900

    レビュー削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "hotels#index"
   resources :hotels, only:[:new, :create, :edit, :update, :show] do
-    resources :reviews, only:[:index, :create]
+    resources :reviews, only:[:index, :create, :destroy]
   end
 end
